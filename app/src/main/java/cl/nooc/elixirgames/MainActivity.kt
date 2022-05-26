f2b118eceaca6ca2ac7ad13dacd23768c6f7cd86
Leonardo Catejo <leo.catejoa@gmail.com>
2022-05-25 21:55:06 -0400

    Finish MainActivity

--- app/src/main/java/cl/nooc/elixirgames/MainActivity.kt
+++ app/src/main/java/cl/nooc/elixirgames/MainActivity.kt
@@ -2,10 +2,21 @@ package cl.nooc.elixirgames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import cl.nooc.elixirgames.databinding.ActivityMainBinding
+import cl.nooc.elixirgames.viewmodel.GamesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<GamesViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getData()
     }
 }
